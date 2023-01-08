4fdc82be1bea4345fa04df85aa6d8ee82886b8a9
ohizz <jtsamuel3084@gmail.com>
2023-01-08 16:55:57 +0100

    nnn

--- routes/routes.js
+++ routes/routes.js
@@ -32,17 +32,5 @@ router.get('/getAll', async (req, res) => {
     }
 })
 
-router.get('/getOne/:id', (req, res) => {
-    res.send('Get by ID API')
-})
-
-router.patch('/update/:id', (req, res) => {
-    res.send('Update by ID API')
-})
-
-
-router.delete('/delete/:id', (req, res) => {
-    res.send('Delete by ID API')
-})
 
 module.exports = router;
