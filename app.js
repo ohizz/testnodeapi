d26b3bcc18e6d3d82d3e6b4ca36544c8f7bdf384
ohizz <jtsamuel3084@gmail.com>
2023-02-13 09:15:56 +0100

    from git

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const Model = require('./models/models');
-mongoose.set('strictQuery', false);
-const routes = require('./routes/routes');
-const path = require('path');
-require('dotenv').config({ path: './.env'});
-const URL = process.env.URL;
-const port = process.env.PORT || 3000;
+const mongoString = process.env.URL;
 
-mongoose.connect(URL);
-const db = mongoose.connection;
+mongoose.connect(mongoString);
+const database = mongoose.connection;
 
-db.on('error', (error) => {
+database.on('error', (error) => {
     console.log(error)
 })
 
-db.once('connected', () => {
+database.once('connected', () => {
     console.log('Database Connected');
 })
-
 const app = express();
-// app.use(express.json());
+app.use(cors())
+app.use(express.json());
+
+const routes = require('./routes/routes');
+
 app.use('/api', routes)
 
-app.listen(port, () => {
-    console.log(`Server Started at ${port}`)
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
 })
